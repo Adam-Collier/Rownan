8e1222aaa120f4ef63c80c4d8e743f7e75502d71
Adam Collier <adamcollierart@gmail.com>
2018-04-09 10:07:27 +0100

    function promisified

--- js/genFunctions/squipImages.js
+++ js/genFunctions/squipImages.js
@@ -3,72 +3,74 @@ let svgToMiniDataURI = require("mini-svg-data-uri");
 let request = require("request-promise-native");
 
 let squipImages = () => {
-  let arr = ["image", "mobile"];
+  return new Promise((resolve, reject) => {
+    let arr = ["image", "mobile"];
 
-  Promise.all(
-    arr.map(s => {
-      return Promise.all(
-        contentData.items.map((x, i) => {
-          if (!contentData.items[i].mobile && s === "mobile") {
-            return;
-          } else {
-            return new Promise((resolve, reject) => {
-              console.log(s);
-              console.log(x.image);
-              request
-                .get({
-                  url: `https://media.missguided.co.uk/image/upload/w_600,q_70/${
-                    x.image
-                  }`,
-                  encoding: "binary"
-                })
-                .then(response => {
-                  fs.writeFile(
-                    `./tempImages/sqip-${s}-temp${i + 1}.jpeg`,
-                    response,
-                    {
-                      encoding: "binary"
-                    },
-                    function(err) {
-                      if (err) throw err;
+    Promise.all(
+      arr.map(s => {
+        return Promise.all(
+          contentData.items.map((x, i) => {
+            if (!contentData.items[i].mobile && s === "mobile") {
+              return;
+            } else {
+              return new Promise((resolve, reject) => {
+                console.log(s);
+                console.log(x.image);
+                request
+                  .get({
+                    url: `https://media.missguided.co.uk/image/upload/w_600,q_70/${
+                      x.image
+                    }`,
+                    encoding: "binary"
+                  })
+                  .then(response => {
+                    fs.writeFile(
+                      `./tempImages/sqip-${s}-temp${i + 1}.jpeg`,
+                      response,
+                      {
+                        encoding: "binary"
+                      },
+                      function(err) {
+                        if (err) throw err;
 
-                      const result = sqip({
-                        filename: path.join(
-                          __dirname,
-                          `../../tempImages/sqip-${s}-temp${i + 1}.jpeg`
-                        ),
-                        numberOfPrimitives: 8
-                      });
+                        const result = sqip({
+                          filename: path.join(
+                            __dirname,
+                            `../../tempImages/sqip-${s}-temp${i + 1}.jpeg`
+                          ),
+                          numberOfPrimitives: 8
+                        });
 
-                      var svg = result.final_svg;
+                        var svg = result.final_svg;
 
-                      var optimizedSVGDataURI = svgToMiniDataURI(svg);
+                        var optimizedSVGDataURI = svgToMiniDataURI(svg);
 
-                      contentData.items[i]["squip" + s] = optimizedSVGDataURI;
-                      resolve(contentData.items[i]["squip" + s]);
-                      //   console.log(optimizedSVGDataURI);
-                      //   console.log("this has been squipped");
-                    }
-                  );
-                })
-                .catch(err => console.log(err));
-            });
+                        contentData.items[i]["squip" + s] = optimizedSVGDataURI;
+                        resolve(contentData.items[i]["squip" + s]);
+                        //   console.log(optimizedSVGDataURI);
+                        //   console.log("this has been squipped");
+                      }
+                    );
+                  })
+                  .catch(err => console.log(err));
+              });
+            }
+          })
+        );
+      })
+    ).then(() => {
+      //   write the JSON file
+      fs.writeFile(
+        path.join(__dirname, "../../output.json"),
+        JSON.stringify(contentData, null, 2),
+        function(err, data) {
+          if (err) {
+            console.log(error);
           }
-        })
-      );
-    })
-  ).then(() => {
-    //   write the JSON file
-    fs.writeFile(
-      path.join(__dirname, "../../output.json"),
-      JSON.stringify(contentData, null, 2),
-      function(err, data) {
-        if (err) {
-          console.log(error);
+          resolve(console.log("JSON file created"));
         }
-        console.log("JSON file created");
-      }
-    );
+      );
+    });
   });
 };
 
