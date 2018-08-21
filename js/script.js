87c145e9c4f453d2a8e8bcfd88d91561941eecd7
Adam Collier <adamcollierart@gmail.com>
2018-08-21 10:18:56 +0100

    remove argument in showInputs, no need to assign sortable to variable

--- js/script.js
+++ js/script.js
@@ -19,14 +19,14 @@ const browserSync = require("./js/browserSync");
 browserSync();
 
 function customEditors(currArea) {
-  var customEditor = currArea.querySelector(".custom");
-  CodeMirror.fromTextArea(customEditor, {
-    matchBrackets: true,
-    mode: "htmlmixed",
-    theme: "one-dark",
-    autoCloseBrackets: true,
-    autoCloseTags: true
-  });
+    var customEditor = currArea.querySelector(".custom");
+    CodeMirror.fromTextArea(customEditor, {
+        matchBrackets: true,
+        mode: "htmlmixed",
+        theme: "one-dark",
+        autoCloseBrackets: true,
+        autoCloseTags: true
+    });
 }
 
 // dropdown menu template literal
@@ -47,76 +47,76 @@ var dropdown = `
 
 // adds a dropdown
 var button = document.querySelector(".button");
-button.addEventListener("click", function() {
-  document
-    .querySelector(".container")
-    .insertAdjacentHTML("beforeend", dropdown);
+button.addEventListener("click", function () {
+    document
+        .querySelector(".container")
+        .insertAdjacentHTML("beforeend", dropdown);
 });
 
 // removes a dropdown
-document.querySelector(".container").addEventListener("click", function(e) {
-  if (e.target.matches(".remove")) {
-    console.log("clicked");
-    e.target.parentNode.remove();
-  }
+document.querySelector(".container").addEventListener("click", function (e) {
+    if (e.target.matches(".remove")) {
+        console.log("clicked");
+        e.target.parentNode.remove();
+    }
 });
 
 // shows the inputs depending on dropdown selection
 // gets the value, goes up to the parent div then appends the appropriate template literal
 function drop(sel) {
-  console.log(sel.value);
-  var s = sel.parentNode.querySelector("div");
-  console.log(s);
-  ind = Array.prototype.slice
-    .call(document.querySelectorAll("select"))
-    .indexOf(sel);
-  showInputs(ind, sel.value, s);
+    console.log(sel.value);
+    var s = sel.parentNode.querySelector("div");
+    console.log(s);
+    ind = Array.prototype.slice
+        .call(document.querySelectorAll("select"))
+        .indexOf(sel);
+    showInputs(sel.value, s);
 }
 
 // menu functions
-ipcRenderer.on("generate", function() {
-  console.log(generate);
-  generate("sqip");
+ipcRenderer.on("generate", function () {
+    console.log(generate);
+    generate("sqip");
 });
-ipcRenderer.on("images", function() {
-  console.log(generate);
-  generate("images");
+ipcRenderer.on("images", function () {
+    generate("images");
 });
-ipcRenderer.on("save", function() {
-  saveToFile();
+ipcRenderer.on("save", function () {
+    saveToFile();
 });
-ipcRenderer.on("preview", function() {
-  preview();
+ipcRenderer.on("preview", function () {
+    preview();
 });
-ipcRenderer.on("mobileView", function() {
-  mobileSize();
+ipcRenderer.on("mobileView", function () {
+    mobileSize();
 });
-ipcRenderer.on("openFile", function() {
-  openFile();
+ipcRenderer.on("openFile", function () {
+    openFile(dropdown);
 });
 
 // switches to the code view
 document
-  .querySelector("svg.preview-button")
-  .addEventListener("click", function() {
-    preview();
-  });
+    .querySelector("svg.preview-button")
+    .addEventListener("click", function () {
+        preview();
+    });
 
+// generated the content
 document.querySelector("#preview").addEventListener("click", generate());
 
 // resizes the window to a mobile view
-document.querySelector(".resize-icon").addEventListener("click", function() {
-  var m = document.querySelector(".resize");
-  mobileSize(m);
+document.querySelector(".resize-icon").addEventListener("click", function () {
+    var m = document.querySelector(".resize");
+    mobileSize(m);
 });
 
-// inputs be drag and dropped to change position
+// inputs can be drag and dropped to change position
 var container = document.getElementById("container");
-var sort = Sortable.create(container, {
-  animation: 150, // ms, animation speed moving items when sorting, `0` — without animation
-  handle: "svg", // Restricts sort start click/touch to the specified element
-  draggable: ".selection", // Specifies which items inside the element should be sortable
-  onUpdate: function(evt /**Event*/) {
-    var item = evt.item; // the current dragged HTMLElement
-  }
+Sortable.create(container, {
+    animation: 150, // ms, animation speed moving items when sorting, `0` — without animation
+    handle: "svg", // Restricts sort start click/touch to the specified element
+    draggable: ".selection", // Specifies which items inside the element should be sortable
+    onUpdate: function (evt /**Event*/) {
+        var item = evt.item; // the current dragged HTMLElement
+    }
 });
